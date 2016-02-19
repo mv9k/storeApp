dc7619d924b6e33317715c6106158a6725aa713f
Wesley Young <wesley.young.portfolio@gmail.com>
2016-02-19 10:17:07 -0700

    Adding and removing categories

--- storeApp/www/js/controllers/accountCtrl.js
+++ storeApp/www/js/controllers/accountCtrl.js
@@ -7,7 +7,7 @@
   angular.module('AcctCtrl', [])
     .controller('AccountCtrl', acctCont);
 
-    acctCont.$inject = ["$scope"];
+    acctCont.$inject = ["$scope", "$ionicPopup"];
     function acctCont($scope){
       var ac = this;
 
@@ -18,6 +18,8 @@
       ac.isLoggedIn=false;
       ac.isCreatingAcc=false;
       ac.usedGoogle=false;
+      ac.showCat = true;
+      ac.emptyCat = true;
 
       //User Info Vars
       ac.email = "";
@@ -26,8 +28,11 @@
       ac.validatedEmail="";
       ac.profileImg="";
       ac.thisUser = {};
-      ac.categories = [];
       ac.newCategory = "";
+      //User lists
+      ac.categories = [];
+      ac.favorites = [];
+      ac.cart = [];
 
       //Function variables
       ac.createAcc=createFireAccount;
@@ -35,6 +40,8 @@
       ac.googleLogin=logInWithGoogle;
       ac.signOut = logOut;
       ac.addCategory = addCat;
+      ac.removeCategory= removeCat;
+      ac.toggleCat=showCategories;
 
       //Create a new account with FireBase
       function createFireAccount(){
@@ -110,11 +117,50 @@
         ac.isLoggedIn=false;
         ac.isCreatingAcc=false;
         ac.usedGoogle=false;
+        ac.showCat = true;
+        ac.emptyCat = true;
       }
       //Add Categories into the user categories array
       function addCat(){
-        ac.categories.push(ac.newCategory);
-        ac.newCategory="";
+        var invalid = false;
+        for(var i=0;i<ac.categories.length;i++){
+          if(ac.newCategory.toUpperCase()==ac.categories[i].key.toUpperCase()){
+            invalid=true;
+          }
+        }
+        if(ac.newCategory==""||ac.newCategory.length>15){
+          invalid=true;
+        }
+        if(!invalid){
+          ac.categories.push({key: ac.newCategory, id: ac.categories.length});
+          ac.newCategory="";
+          $("#newCategoryInput").css("border", "solid lightgrey 1px")
+        }
+        else{
+          $("#newCategoryInput").css("border", "solid red 1px")
+        }
+        if(ac.categories.length>0){
+          ac.emptyCat=false;
+        }
+      }
+      function removeCat(id){
+        for(var i=0;i<ac.categories.length;i++){if(id==ac.categories[i].id){ac.categories.splice(i, 1)}}
+      }
+      function addCart(){
+
+      }
+      function removeCart(){
+
+      }
+      function addFav(){
+
+      }
+      function removeFav(){
+
+      }
+      function showCategories(){
+        ac.showCat?ac.showCat=false:ac.showCat=true;
+        ac.showCat?$("#toggleCat").html("Hide Categories"):$("#toggleCat").html("Show Categories");
       }
     }
 }());
