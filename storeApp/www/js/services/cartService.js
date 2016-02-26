b6fa8dea5bd1373ba9e1672ab176423e01e4b7f0
Gabe Driggs <gabe.driggs@gmail.com>
2016-02-26 07:47:57 -0700

    sadfdsaadsf

--- storeApp/www/js/services/cartService.js
+++ storeApp/www/js/services/cartService.js
@@ -5,7 +5,7 @@ angular.module('cartService', [])
 
   .service('cartService',cartService);
 
-function cartService(){
+function cartService() {
 
   var cs = this;
 
@@ -15,27 +15,30 @@ function cartService(){
   cs.plzClick = plzClick;
 
 
-  function plzClick(){
-    alert("you've reached the service")
+  function plzClick() {
+    alert("you've reached the service");
 
-  cs.buyIt = buyIt;
+    cs.buyIt = buyIt;
 
-  function buyIt(product){
-    alert();
-    alert(product.name);
-    //window.open('http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D' + product.itemId + '%7C1%26affp1%3DM1u8aZZoZbep0p3P7hVn_sT4Ry97xPSOvnILkAKRCH8%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi')
+    function buyIt(product) {
+      alert();
+      alert(product.name);
+      //window.open('http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D' + product.itemId + '%7C1%26affp1%3DM1u8aZZoZbep0p3P7hVn_sT4Ry97xPSOvnILkAKRCH8%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi')
 
-  }
-  //function addToCart(product){
-  //  alert('did it work')
-  //}
-  function addToCart(product){
-    cs.cartProducts.push(product);
-    console.log(product);
+    }
+
+    //function addToCart(product){
+    //  alert('did it work')
+    //}
+    function addToCart(product) {
+      cs.cartProducts.push(product);
+      console.log(product);
 
 
-  }
-  function deleteCurrent(currIndex){
-    cs.cartProducts.splice(currIndex,1)
+    }
+
+    function deleteCurrent(currIndex) {
+      cs.cartProducts.splice(currIndex, 1)
+    }
   }
 }
